73057189cc0ae5c824930a3dcffda0d8b48b82ed
p33r-positron <84068153+p33r-positron@users.noreply.github.com>
2022-02-23 16:06:46 +0100

    Oups... Bug fixé

--- universalTCP.js
+++ universalTCP.js
@@ -1,7 +1,7 @@
 function welnetTCP(host, port, data, msgCallback, closeCallback, ws2sServer)
 {
-	let ws2sHost = typeof ws2sServer !== "string" || !ws2sServer ? ws2sServer : "wss://ws2s.feling.net/"; //We need a ws2s server. See https://github.com/playay/ws2s
-	let socket = new WebSocket(ws2sHost)
+	const ws2sHost = typeof ws2sServer !== "undefined" ? ws2sServer : "wss://ws2s.feling.net/"; //We need a ws2s server. See https://github.com/playay/ws2s
+	const socket = new WebSocket(ws2sHost)
 	socket.onmessage = msgCallback;
 	socket.onopen = function()
 	{
